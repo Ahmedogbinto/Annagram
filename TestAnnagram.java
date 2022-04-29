00ddd39572f02b1b6b20b8311a2070419f555d43
AhmedOg <ahmedogbinto@gmail.com>
2022-04-29 14:30:26 -0400

    Annagram TestCase

---
+++ TestAnnagram.java
@@ -0,0 +1,13 @@
+public class TestAnnagram {
+    public void replaceFillTheSpace()  throws Exception{
+        String str="Morning everyone";
+        String fill=str.replace(" ","");
+
+        assertEquals("Morningeveryone",fill );
+
+
+    }
+
+    private void assertEquals(String morningeveryone, String fill) {
+    }
+}
--- out/production/Annagram/README.md
+++
@@ -1,2 +0,0 @@
-# Annagram
-The anagram is a word that is obtained by allowing a character of a word or a first expression. Example Peek and keep - Java and vaaj
